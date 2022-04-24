88a1dac643cefd24939c589e425ac3f6499cdc0e
PadNim14 <PadNim14@github.com>
2022-04-24 11:50:34 -0400

    renamed survey elements

--- src/index.js
+++ src/index.js
@@ -9,10 +9,9 @@ import {firebaseInitial, db} from './config.js';
 
 Survey.StylesManager.applyTheme("darkrose");
 
-var surveyJSON = { title: "Enter New Client", description: "Use this questionnaire to enter any new clients into the database.",
+var surveyJSON = { title: "Enter New Patient", description: "Use this questionnaire to enter new patients into the database.",
 pages: [{ name: "page1",
-elements: [{ type: "text", name: "question1", title: "First Name", isRequired: true, requiredErrorText: "Enter first name." },
-{ type: "text", name: "question10", title: "Last Name", isRequired: true, requiredErrorText: "Enter last name." },
+elements: [{ type: "text", name: "question1", title: "Full Name", isRequired: true, requiredErrorText: "Enter full name." },
 { type: "radiogroup", name: "question11", title: "Gender",
 choices: [{ value: "item1", text: "Female" }, { value: "item2", text: "Male" }, { value: "item3", text: "Other" }, { value: "item4", text: "Prefer not to say" }] },
 { type: "text", name: "question10Other", title: "Age", isRequired: true, requiredErrorText: "Enter age"},
@@ -23,23 +22,19 @@ validators:
     text: 'Enter a valid phone number',
     regex: "^(\\+\\d{1,2}\\s)?\\(?\\d{3}\\)?[\\s.-]\\d{3}[\\s.-]\\d{4}$"
     }},
-{ type: "text", name: "question5", title: "Emergency Contact", isRequired: true, requiredErrorText: "Enter emergency contact"},
+{ type: "text", name: "question5", title: "Emergency Contact 1", isRequired: true, requiredErrorText: "Enter emergency contact 1"},
+{ type: "text", name: "question5Other", title: "Emergency Contact 2", isRequired: true, requiredErrorText: "Enter emergency contact 2"},
 { type: "text", name: "question3", title: "Email Address", isRequired: true, requiredErrorText: "Enter email address",
 validators: 
 {
     type: "email"
 } },
 { type: "text", name: "question4", title: "Street Address", isRequired: true, requiredErrorText: "Enter street address" },
-{ type: "text", name: "question6", title: "Zip Code", isRequired: true, requiredErrorText: "Enter zip code.",
-validators:
-{
-    type: "regex",
-    text: "Enter valid zip code.",
-    regex: "^\d{5}(?:[-\s]\d{4})?$"
-} },
-{ type: "comment", name: "question7", title: "Description of Ailment" },
-{ type: "radiogroup", name: "question8", title: "Type of Care", choices: [{ value: "item1", text: "Frequent" }, { value: "item2", text: "Infrequent" }, { value: "item3", text: "Need-Based" }] },
-{ type: "comment", name: "question9", title: "Other Important Information" }] }] }
+{ type: "text", name: "question6", title: "County", isRequired: true, requiredErrorText: "Enter county" },
+{ type: "text", name: "question7", title: "Primary Diagonsis" },
+{ type: "text", name: "question8", title: "Secondary Diagonsis" },
+{ type: "radiogroup", name: "question9", title: "Type of Care", choices: [{ value: "item1", text: "Frequent" }, { value: "item2", text: "Infrequent" }, { value: "item3", text: "Need-Based" }] },
+{ type: "comment", name: "question10", title: "Other Important Information" }] }] }
 
 
 function sendDataToServer(survey) {
@@ -47,16 +42,16 @@ function sendDataToServer(survey) {
 
     //db.collection("nurses").document(nurses.nursesId).collection("patients_profiles").
     db.collection("Patient Profiles").add({
-        First_Name: JSON.stringify(survey.data["question1"]),
-        Last_Name: JSON.stringify(survey.data["question10"]),
+        Name: JSON.stringify(survey.data["question1"]),
         Gender: JSON.stringify(survey.data["question11"]),
         Age: JSON.stringify(survey.data["question10Other"]),
         Phone_Number: JSON.stringify(survey.data["question2"]),
-        Emergency_Contact: JSON.stringify(survey.data["question5"]),
-        Email: JSON.stringify(survey.data["question3"]),
+        Emergency_Contact1: JSON.stringify(survey.data["question5"]),
+        Emergency_Contact2: JSON.stringify(survey.data["question5Other"]),
         Street_Address: JSON.stringify(survey.data["question4"]),
-        Zip_Code: JSON.stringify(survey.data["question6"]),
-        Description_of_Ailment: JSON.stringify(survey.data["question7"]),
+        County: JSON.stringify(survey.data["question6"]),
+        Primary_Diagnonsis: JSON.stringify(survey.data["question7"]),
+        Secondary_Diagnonsis: JSON.stringify(survey.data["question8"]),
         Type_of_Care: JSON.stringify(survey.data["question8"]),
         Other_Important_Information: JSON.stringify(survey.data["question9"]),
     })
